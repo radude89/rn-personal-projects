e3ac3a47f849e1c6390e33445ba3e7a9b4c70ebd
radude89 <radu.ionut.dan@gmail.com>
2024-02-15 06:03:09 +0200

    Active job type implementation

--- second_project/second-app/components/home/welcome/Welcome.jsx
+++ second_project/second-app/components/home/welcome/Welcome.jsx
@@ -5,10 +5,11 @@ import { useRouter } from 'expo-router';
 import styles from './welcome.style'
 import { icons, SIZES } from '../../../constants';
 
-// const jobTypes = ["Full-time", "Part-time", "Contractor"];
+const jobTypes = ["Full-time", "Part-time", "Contractor"];
 
 const Welcome = () => {
   const router = useRouter();
+  const [activeJobType, setActiveJobType] = useState('Full-time')
 
   return (
     <View>
@@ -35,6 +36,16 @@ const Welcome = () => {
         </TouchableOpacity>
       </View>
       <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => (
+            <TouchableOpacity
+              style={styles.tab(activeJobType, item)}
+            >
+              <Text>{item}</Text>
+            </TouchableOpacity>
+          )}
+        />
         
       </View>
     </View>
