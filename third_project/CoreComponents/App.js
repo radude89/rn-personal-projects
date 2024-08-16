f11e4cbca8e0ddb818e9e6e66d46f727d0539367
radude89 <radu.ionut.dan@gmail.com>
2024-08-16 16:47:54 +0300

    Recap CoreComponents

--- third_project/CoreComponents/App.js
+++ third_project/CoreComponents/App.js
@@ -1,15 +1,25 @@
-import { View, Text, Image, ImageBackground } from 'react-native'
-const logoImg = require("./assets/adaptive-icon.png")
+import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 
 export default function App() {
-  return <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-    {/* <Image source={logoImg} style={{ width: 300, height: 300 }} />
-    <Image 
-      source={{ uri: "https://picsum.photos/300" }}
-      style={{ width: 300, height: 300 }}
-    /> */}
-    <ImageBackground source={logoImg} style={{ flex: 1 }}>
-      <Text>IMAGE TEXT</Text>
-    </ImageBackground>
-  </View>
+  return (
+    <View style={styles.container}>
+      <Text>
+        <Text style={{ color: 'white' }}>Hello</Text>, Radu!
+      </Text>
+        <View style={{ width: 100, height: 100, backgroundColor: 'lightblue' }} />
+        <Image 
+          source={{ uri: 'https://picsum.photos/200' }} 
+          style={{ width: 100, height: 100 }} 
+        />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
