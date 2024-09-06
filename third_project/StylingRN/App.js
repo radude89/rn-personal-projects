2746b1f5978670511413e931ef4cb944d2ad11b3
radude89 <radu.ionut.dan@gmail.com>
2024-09-06 05:04:19 +0300

    Complete Styles project

--- third_project/StylingRN/App.js
+++ third_project/StylingRN/App.js
@@ -3,14 +3,23 @@ import { View, Text, StyleSheet } from 'react-native'
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.darkMode}>
+        <Text style={styles.darkModeText}>
+          Style inheritance<Text style={styles.boldText}> in bold</Text>
+        </Text>
+      </View>
+      
       <Text style={styles.title}>StyleSheet API</Text>
+      
       <View 
         style={[
           styles.box,
-          styles.lightBlueBox
+          styles.lightBlueBox,
+          styles.boxShadow,
+          styles.androidShadow
         ]}
       >
-        <Text>Light-blue box</Text>
+        <Text style= {{ borderRadius: 5, backgroundColor: "red" }}>Light-blue box</Text>
       </View>
       
       <View 
@@ -29,15 +38,41 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1, backgroundColor: 'plum', padding: 60
   },
+  darkMode: {
+    backgroundColor: 'black',
+  },
+  darkModeText: {
+    color: 'white'
+  },
+  boldText: {
+    fontWeight: 'bold'
+  },
   box: {
-    width: 100,
-    height: 100,
-    padding: 10
+    width: 250,
+    height: 250,
+    paddingHorizontal: 10,
+    paddingVertical: 20,
+    marginVertical: 20,
+    borderWidth: 2,
+    borderColor: "purple",
+    borderRadius: 5
   },
   lightBlueBox: {
     backgroundColor: 'lightblue'
   },
   lightGreenBox: {
     backgroundColor: 'lightgreen'
+  },
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      width: 6, 
+      height: 6
+    },
+    shadowOpacity: 0.6,
+    shadowRadius: 4
+  },
+  androidShadow: {
+    elevation: 10
   }
 });
