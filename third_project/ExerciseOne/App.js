b69d0b2619b42289785de4c4336466255819c21e
radude89 <radu.ionut.dan@gmail.com>
2024-09-26 05:21:24 +0300

    Implement part 3 of 6 for PokemonCards

--- third_project/ExerciseOne/App.js
+++ third_project/ExerciseOne/App.js
@@ -6,9 +6,18 @@ import {
 import PokemonCard from './components/PokemonCard';
 
 export default function App() {
+  const charmanderData = {
+    name: 'Charmander',
+    image: require('./assets/charmander.png'),
+    type: 'Fire',
+    hp: 39,
+    moves: ['Scratch', 'Ember', 'Growl', 'Leer'],
+    weaknesses: ['Water', 'Rock'],
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <PokemonCard />
+      <PokemonCard {...charmanderData} />
     </SafeAreaView>
   );
 }
