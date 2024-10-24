f05de342629d164ccdb02e730aa4f5cd54c5bbc0
radude89 <radu.ionut.dan@gmail.com>
2024-10-24 05:24:52 +0300

    Add list component with data fetched from the network

--- third_project/RNNetworking/App.js
+++ third_project/RNNetworking/App.js
@@ -1,20 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, StatusBar, View, FlatList, Text } from 'react-native';
+import { useState, useEffect } from 'react';
 
 export default function App() {
+
+  const [postList, setPostList] = useState([]);
+
+  const fetchData = async (limit = 10) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+    );
+    const data = await response.json();
+    setPostList(data);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);;
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.listContainer}>
+        <FlatList
+          data={postList}
+          renderItem={({ item }) => {
+            return (
+              <View style={styles.card}>
+                <Text style={styles.titleText}>{item.title}</Text>
+                <Text style={styles.bodyText}>{item.body}</Text>
+              </View>
+            );
+          }}
+          ItemSeparatorComponent={() => <View style={{height: 16 }} />}
+          ListEmptyComponent={<Text>No Posts Found</Text>}
+          ListHeaderComponent={<Text style={styles.headerText}>Post List</Text>}
+          ListFooterComponent={<Text style={styles.footerText}>End of list</Text>}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f5f5f5',
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: StatusBar.currentHeight,
   },
+  listContainer: {
+    flex: 1,
+    paddingHorizontal: 16
+  },
+  card: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    borderWidth: 1,
+  },
+  titleText: {
+    fontSize: 30
+  },
+  bodyText: {
+    fontSize: 24,
+    color: "#666666"
+  },
+  headerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginBottom: 12,
+    marginTop: 12
+  },
+  footerText: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 12
+  }
 });
