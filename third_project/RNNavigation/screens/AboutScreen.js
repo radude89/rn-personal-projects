7df34f56dcacf9378ace627f9d0d39dc61303203
radude89 <radu.ionut.dan@gmail.com>
2025-01-10 04:50:40 +0200

    Start project RNNavigation

---
+++ third_project/RNNavigation/screens/AboutScreen.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function AboutScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>About Screen</Text>
+            <Button 
+                title="Go to Home"
+                onPress={() => navigation.navigate('Home')}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+});
---
+++ third_project/RNNavigation/screens/HomeScreen.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+
+export default function HomeScreen({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Home Screen</Text>
+            <Button 
+                title="Go to About" 
+                onPress={() => navigation.navigate('About')} 
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    text: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 16,
+    },
+});
