a9a0c350dac86838ec29db315846c7627d8e596e
radude89 <radu.ionut.dan@gmail.com>
2025-01-16 04:55:21 +0200

    Navigation in navigation

--- third_project/RNNavigation/App.js
+++ third_project/RNNavigation/App.js
@@ -4,6 +4,7 @@ import CourseListScreen from './screens/CourseListScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { AboutStack } from './AppStack';
 
 const Tab = createBottomTabNavigator()
 
@@ -29,6 +30,13 @@ export default function App() {
                     }}
                 />
                 <Tab.Screen name="Settings" component={SettingsScreen} />
+                <Tab.Screen 
+                    name="About Stack" 
+                    component={AboutStack} 
+                    options={{
+                        headerShown: false
+                    }}
+                />
             </Tab.Navigator>
         </NavigationContainer>
     );
--- third_project/RNNavigation/AppStack.js
+++ third_project/RNNavigation/AppStack.js
@@ -7,48 +7,54 @@ import { Text } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
+export const AboutStack = () => {
+  return (
+    <Stack.Navigator 
+      initialRouteName='Home'
+      screenOptions={{ 
+        headerShown: true,
+        headerStyle: {
+          backgroundColor: '#6a51ae'
+        },
+        headerTintColor: '#fff',
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        },
+        contentStyle: {
+          backgroundColor: '#e8e4f3'
+        },
+        headerRight: () => (
+          <Pressable 
+            onPress={() => alert('Menu pressed')}
+          >
+            <Text style={{ color: '#fff', fontSize: 16}}>Menu</Text>
+          </Pressable>
+        ),
+      }}
+    >
+      <Stack.Screen 
+        name="Home" 
+        component={HomeScreen}
+        options={{ 
+          title: 'Welcome Home',
+        }}
+      />
+      <Stack.Screen 
+        name="About"
+        component={AboutScreen} 
+        initialParams={{ name: 'Guest' }}
+        // options={({ route }) => ({
+        //   title: route.params.name,
+        // })}
+      />
+    </Stack.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-        initialRouteName='Home'
-        screenOptions={{ 
-          headerShown: true,
-          headerStyle: {
-            backgroundColor: '#6a51ae'
-          },
-          headerTintColor: '#fff',
-          headerTitleStyle: {
-            fontWeight: 'bold'
-          },
-          contentStyle: {
-            backgroundColor: '#e8e4f3'
-          },
-          headerRight: () => (
-            <Pressable 
-              onPress={() => alert('Menu pressed')}
-            >
-              <Text style={{ color: '#fff', fontSize: 16}}>Menu</Text>
-            </Pressable>
-          ),
-        }}
-      >
-        <Stack.Screen 
-          name="Home" 
-          component={HomeScreen}
-          options={{ 
-            title: 'Welcome Home',
-          }}
-        />
-        <Stack.Screen 
-          name="About"
-          component={AboutScreen} 
-          initialParams={{ name: 'Guest' }}
-          // options={({ route }) => ({
-          //   title: route.params.name,
-          // })}
-        />
-      </Stack.Navigator>
+      <AboutStack />
     </NavigationContainer>
   );
 }
