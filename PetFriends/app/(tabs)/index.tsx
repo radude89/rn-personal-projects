4cfabfa0fd15d891f169ea381533d6e76491083b
radude89 <radu.ionut.dan@gmail.com>
2025-02-06 08:15:17 +0100

    Learning Chapter 5 - Create a modal

--- PetFriends/app/(tabs)/index.tsx
+++ PetFriends/app/(tabs)/index.tsx
@@ -3,6 +3,12 @@ import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
 import * as ImagePicker from 'expo-image-picker';
 import { useState } from "react";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
+import { ImageSource } from "expo-image";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require('../../assets/images/background-image.png');
 
@@ -10,6 +16,9 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(undefined);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -19,27 +28,79 @@ export default function Index() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
       console.log(result);
     } else {
       alert("You did not select any image.");
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onSaveImageAsync = async () => {
+
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer imgSource={selectedImage || PlaceholderImage} />
-      </View>
+        {pickedEmoji && (
+          <EmojiSticker 
+            imageSize={40} 
+            stickerSource={pickedEmoji} 
+          />  
+        )}
 
-      <View style={styles.footerContainer}>
-        <Button 
-          onPress={pickImageAsync}
-          label={"Choose a photo"} 
-          theme="primary"
-        />
-        
-        <Button label={"Use this photo"} />
       </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton 
+              icon="refresh" 
+              label="Reset" 
+              onPress={onReset} 
+            />
+            
+            <CircleButton onPress={onAddSticker} />
+            
+            <IconButton 
+              icon="save-alt" 
+              label="Save" 
+              onPress={onSaveImageAsync} 
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button 
+            onPress={pickImageAsync}
+            label={"Choose a photo"} 
+            theme="primary"
+          />
+          
+          <Button 
+            label={"Use this photo"} 
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList 
+          onSelect={setPickedEmoji} 
+          onCloseModal={onModalClose}
+        />
+      </EmojiPicker>
     </View>
   );
 }
@@ -52,10 +113,18 @@ const styles = StyleSheet.create({
 
   },
   imageContainer: {
-    flex: 1
+    flex: 1,
   },
   footerContainer: {
     flex: 0.25,
     alignItems: 'center'
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row'
   }
 });
--- PetFriends/components/ImageViewer.tsx
+++ PetFriends/components/ImageViewer.tsx
@@ -16,8 +16,8 @@ export default function ImageViewer({ imgSource }: Props) {
 
 const styles = StyleSheet.create({
   image: {
-    width: 320,
-    height: 440,
+    width: 300,
+    height: 300,
     borderRadius: 18
   },
 });
